8ae2fc037589bcf4a056737db27cb4793eca4a2e
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-26 10:16:43 +0600

    Creating dynamic Card with Props and css styles

--- src/App.js
+++ src/App.js
@@ -10,7 +10,62 @@ function App() {
         <Player name="Leo Messi" club="FC Barcelona" />
         <Player name="Suarez" club="Athletico Madrid" />
         <Player name="Neymar" club="Paris Saint Germain" />
+        <ProductCard headingTitle="Photoshop" price="30.99" />
+        <ProductCard headingTitle="Visual Studio Code" price="9.99" />
+        <ProductCard headingTitle="Adobe XD" price="14.99" />
       </header>
+    </div>
+  );
+}
+
+function ProductCard(props) {
+  const cardStyle = {
+    marginTop: "20px",
+    boxSizing: "border-box",
+    textAlign: "left",
+    padding: "20px",
+    color: "#4f4f4f",
+    width: "300px",
+    border: "1px solid #cfcbc2",
+    backgroundColor: "#d9dbdb",
+    borderRadius: "10px",
+  };
+
+  const bottomCard = {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "40px",
+  };
+
+  const buttonStyle = {
+    width: "80px",
+    backgroundColor: "#32488a",
+    color: "white",
+    fontWeight: "bold",
+    border: "none",
+    borderRadius: "40px",
+  };
+
+  return (
+    <div style={cardStyle} className="productCard">
+      <h5>{props.headingTitle}</h5>
+      <h3>US${props.price}/mo</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde,
+        laboriosam!
+      </p>
+      <a href="#">See Plan & pricing details</a>
+      <div style={bottomCard} className="bottom">
+        <input type="checkbox" name="check" id="check" />
+        <label
+          style={{ fontSize: "13px", position: "relative", right: "15px" }}
+          htmlFor="check"
+        >
+          Add Adobe Stock <br />
+          Get one month free*
+        </label>
+        <button style={buttonStyle}>Buy Now</button>
+      </div>
     </div>
   );
 }
