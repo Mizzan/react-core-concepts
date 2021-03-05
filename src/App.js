b224e7755208ca2d2265d78ca1e990bbdf953607
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-05 16:54:56 +0600

    Using material ui into react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import News from "./components/News/News";
-
-function handleError(err) {
-  return console.log("Oh Noooo", err);
-}
+import { Button } from "@material-ui/core";
+import RecipeReviewCard from "./components/Cards/Cards";
 
 function App() {
-  const apiKey = "ecd781a4f8f0456abf023a72e84a444c";
-  const [news, setNews] = useState([]);
-  useEffect(() => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
-      .then((res) => res.json())
-      .then((newsData) => setNews(newsData.articles))
-      .catch(handleError);
-  }, []);
   return (
-    <div className="container">
-      <h1>Newspaper</h1>
-      <div className="row">
-        <div className="col">
-          <News news={news}></News>
-        </div>
-      </div>
-    </div>
+    <>
+      <Button variant="contained" color="secondary">
+        Click
+      </Button>
+
+      <RecipeReviewCard></RecipeReviewCard>
+    </>
   );
 }
 
